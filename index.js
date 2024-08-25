789d9c5031fc3c01209bf3af82c17cb34d8ba08d
IvanR96 <irios96@hotmail.com>
2024-08-24 23:37:33 -0700

    main js file

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.post("/get-secret", async (req, res) => {
 app.post("/post-secret", async (req, res) => {
   // TODO 2: Use axios to POST the data from req.body to the secrets api servers.
   try{
-    const result = await axios.post(API_URL + "/secrets", req.body, config);
+    const result = await axios.post(API_URL + "/secrets/", req.body, config);
     res.render("index.ejs", { content: JSON.stringify(result.data) });
   }
   catch(error){
@@ -60,6 +60,7 @@ app.post("/put-secret", async (req, res) => {
 app.post("/patch-secret", async (req, res) => {
   const searchId = req.body.id;
   // TODO 4: Use axios to PATCH the data from req.body to the secrets api servers.
+
 });
 
 app.post("/delete-secret", async (req, res) => {
